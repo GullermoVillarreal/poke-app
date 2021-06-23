6f97ee7479389de92a699f282388b84fb0bbd788
memowars <gvillareal@guaodev.com>
2021-06-22 23:11:40 -0500

    Configuracion

---
+++ src/PokeApp.tsx
@@ -0,0 +1,11 @@
+import SideBar from "./components/SideBarMenu";
+function PokeApp() {
+  return (
+    <div>
+      <SideBar />
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default PokeApp;
---
+++ src/components/SideBarMenu.tsx
@@ -0,0 +1,10 @@
+const SideBarMenu = () => {
+  return (
+    <div className="">
+      <h1>Sidebar aqi</h1>
+      <img src="./public/assets/avatar" alt="" />
+    </div>
+  );
+};
+
+export default SideBarMenu;
